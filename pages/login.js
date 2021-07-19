ad33e8e54d9d0a8721bffc18266e772b5f6084a2
Thyago Fontenele <thyagofontenele@id.uff.br>
2021-07-19 18:20:34 -0300

    Finalizado

--- pages/login.js
+++ pages/login.js
@@ -5,8 +5,8 @@ import nookies from 'nookies';
 
 export default function LoginScreen() {
   const router = useRouter();
-  const [githubUser, setGithubUser] = React.useState('omariosouto');
-
+  const [githubUser, setGithubUser] = React.useState('ThyagoFontenele');
+  
   return (
     <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <div className="loginScreen">
